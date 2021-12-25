6a5bed48474c2e9c99c17153312e91a0ac3434c9
Saffat Aziz <saziz028@uottawa.ca>
2021-12-25 15:26:18 -0500

    changes

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ import { AiOutlineLogout } from 'react-icons/ai';
 import { useParams, useNavigate } from 'react-router-dom';
 import { GoogleLogout } from 'react-google-login';
 const UserProfile = () => {
- return <div></div>;
+ return <div>User Profile</div>;
 };
 
 export default UserProfile;
