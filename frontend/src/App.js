210a0b2ddc3dbce102530ef8ab7f09861394eb27
Saffat Aziz <saziz028@uottawa.ca>
2021-12-24 22:07:30 -0500

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
 function App() {
  return (
   <Routes>
