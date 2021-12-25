679080a96ce62b73accbb81ad7aeb09f31a4e420
Saffat Aziz <saziz028@uottawa.ca>
2021-12-25 15:51:23 -0500

    Update Home.jsx

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -8,14 +8,18 @@ import { userQuery } from '../utils/data';
 
 import logo from '../assets/logo-black.png';
 import Pins from './Pins';
+
 const Home = () => {
  const [toggleSidebar, setToggleSidebar] = useState(false);
  const [user, setUser] = useState();
  const scrollRef = useRef(null);
+
  const userInfo =
   localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
  useEffect(() => {
   const query = userQuery(userInfo?.googleId);
+
   client.fetch(query).then((data) => {
    setUser(data[0]);
   });
@@ -24,19 +28,20 @@ const Home = () => {
  useEffect(() => {
   scrollRef.current.scrollTo(0, 0);
  });
+
  return (
   <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
    <div className='hidden md:flex h-screen flex-initial'>
     <Sidebar user={user && user} />
    </div>
-   <div className='"flex md:hidden flex-row'>
+   <div className='flex md:hidden flex-row'>
     <div className='p-2 w-full flex flex-row justify-between items-center shadow-md'>
      <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(true)} />
      <Link to='/'>
       <img src={logo} alt='logo' className='w-28' />
      </Link>
      <Link to={`user-profile/${user?._id}`}>
-      <img src={user?.image} alt='user-pic' className='w-9 h-9 rounded-full m-1' />
+      <img src={user?.image} alt='user-pic' className='w-9 h-9 rounded-full ' />
      </Link>
     </div>
     {toggleSidebar && (
