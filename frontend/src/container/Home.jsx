dccf47bb79395121538f17df315575f6f4d653ae
Saffat Aziz <saziz028@uottawa.ca>
2021-12-25 01:43:07 -0500

    changes

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -16,15 +16,18 @@ const Home = () => {
   localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
  useEffect(() => {
   const query = userQuery(userInfo?.googleId);
-
   client.fetch(query).then((data) => {
    setUser(data[0]);
   });
  }, []);
+
+ useEffect(() => {
+  scrollRef.current.scrollTo(0, 0);
+ });
  return (
   <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
    <div className='hidden md:flex h-screen flex-initial'>
-    <Sidebar />
+    <Sidebar user={user && user} />
    </div>
    <div className='"flex md:hidden flex-row'>
     <div className='p-2 w-full flex flex-row justify-between items-center shadow-md'>
@@ -36,20 +39,20 @@ const Home = () => {
       <img src={user?.image} alt='user-pic' className='w-9 h-9 rounded-full m-1' />
      </Link>
     </div>
-    {/* {toggleSidebar && (
+    {toggleSidebar && (
      <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
       <div className='absolute w-full flex justify-end items-center p-2'>
        <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSidebar(false)} />
       </div>
       <Sidebar closeToggle={setToggleSidebar} user={user && user} />
      </div>
-    )} */}
+    )}
    </div>
-   <div className='pb-2 flex-1 h-screen overflow-y-scroll'>
-    {/* <Routes>
+   <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+    <Routes>
      <Route path='/user-profile/:userId' element={<UserProfile />} />
      <Route path='/*' element={<Pins user={user && user} />} />
-    </Routes> */}
+    </Routes>
    </div>
   </div>
  );
