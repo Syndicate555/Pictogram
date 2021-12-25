8297c35e79dcafc3d43e2a8f4918635b4e70bfcb
Saffat Aziz <saziz028@uottawa.ca>
2021-12-25 16:34:41 -0500

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,16 +5,16 @@ import { IoIosArrowForward } from 'react-icons/io';
 import logo from '../assets/logo-black.png';
 
 const isNotActiveStyle =
- 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+ 'flex items-center px-5 gap-3 hover:bg-sky-100 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 const isActiveStyle =
- 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize';
+ 'flex items-center px-5 gap-3 hover:bg-sky-100 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize';
 
 const categories = [
  { name: 'Animals' },
  { name: 'Cars' },
  { name: 'Nature' },
  { name: 'Technology' },
- { name: 'Real Estate' },
+ { name: 'Architecture' },
  { name: 'Photography' },
  { name: 'Travel' },
  { name: 'Lifestyle' },
