fb3418ee39e441b4057dd69baaa9a80a1307dc2b
Saffat Aziz <saziz028@uottawa.ca>
2021-12-24 23:43:34 -0500

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,17 +11,18 @@ import { FcGoogle } from 'react-icons/fc';
 const Login = () => {
  const navigate = useNavigate();
  const responseGoogle = (response) => {
-  // localStorage.setItem('user', JSON.stringify(response.profileObj));
-  // const { name, googleId, imageUrl } = response.profileObj;
-  // const doc = {
-  //  _id: googleId,
-  //  _type: 'user',
-  //  userName: name,
-  //  image: imageUrl,
-  // };
-  // client.createIfNotExists(doc).then(() => {
-  //  navigate('/', { replace: true });
-  // });
+  console.log(response);
+  localStorage.setItem('user', JSON.stringify(response.profileObj));
+  const { name, googleId, imageUrl } = response.profileObj;
+  const doc = {
+   _id: googleId,
+   _type: 'user',
+   userName: name,
+   image: imageUrl,
+  };
+  client.createIfNotExists(doc).then(() => {
+   navigate('/', { replace: true });
+  });
  };
 
  return (
@@ -37,9 +38,9 @@ const Login = () => {
      className='w-full h-full object-cover'
     />
 
-    <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay'>
+    <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0  	  bg-blackOverlay'>
      <div className='p-5'>
-      <img src={logo} width='130px' />
+      <img src={logo} width='330px' />
      </div>
 
      <div className='shadow-2xl'>
