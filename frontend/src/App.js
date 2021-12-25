fc440ad537a35ca4ff758cccfe794ac001f3d1d0
Saffat Aziz <saziz028@uottawa.ca>
2021-12-24 22:53:29 -0500

    bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
@@ -6,7 +5,7 @@ function App() {
  return (
   <Routes>
    <Route path='login' element={<Login />} />
-   <Route path='/*' element={<Home />} />
+   {/* <Route path='/*' element={<Home />} /> */}
   </Routes>
  );
 }
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,14 +1,16 @@
+import logo from '../assets/default.png';
+import shareVideo from '../assets/video.mp4';
+
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
-import shareVideo from '../assets/video.mp4';
-import logo from '../assets/default.png';
+
+// import { client } from '../client';
 
 const Login = () => {
  const navigate = useNavigate();
  const responseGoogle = (response) => {
-  console.log(response);
   // localStorage.setItem('user', JSON.stringify(response.profileObj));
   // const { name, googleId, imageUrl } = response.profileObj;
   // const doc = {
@@ -21,9 +23,10 @@ const Login = () => {
   //  navigate('/', { replace: true });
   // });
  };
+
  return (
   <div className='flex justify-start items-center flex-col h-screen'>
-   <div className='relative w-full h-ful'>
+   <div className=' relative w-full h-full'>
     <video
      src={shareVideo}
      type='video/mp4'
@@ -33,10 +36,12 @@ const Login = () => {
      autoPlay
      className='w-full h-full object-cover'
     />
+
     <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay'>
      <div className='p-5'>
-      <img src={logo} width='330px' />
+      <img src={logo} width='130px' />
      </div>
+
      <div className='shadow-2xl'>
       <GoogleLogin
        clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
@@ -50,9 +55,9 @@ const Login = () => {
          <FcGoogle className='mr-4' /> Sign in with google
         </button>
        )}
-       // onSuccess={responseGoogle}
-       // onFailure={responseGoogle}
-       // cookiePolicy='single_host_origin'
+       onSuccess={responseGoogle}
+       onFailure={responseGoogle}
+       cookiePolicy='single_host_origin'
       />
      </div>
     </div>
